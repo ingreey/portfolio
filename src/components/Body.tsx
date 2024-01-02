23071e339f3b21a0fd882de7b2a46d4c78559ce5
nickeym <thenikkila@gmail.com>
2024-01-01 22:20:36 -0800

    updated socials

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,30 +4,24 @@ import gsap from "gsap";
 
 function Body() {
   const comp = useRef(null);
+  const handleEmailClick = () => {
+    window.location.href = "mailto:greeyworks@gmail.com";
+  };
+
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       const t1 = gsap.timeline();
-      t1.from("#intro-slider", {
-        xPercent: "-100",
-        duration: 1.3,
-        delay: 0.3,
-      })
-        .to("#intro-slider", {
-          xPercent: "110",
-          duration: 1.3,
-          delay: 0.3,
-        })
-        .from("#main", {
-          opacity: 0,
-          y: "-=30",
-          delay: 0.3,
-        });
+      t1.from("#main", {
+        opacity: 0,
+        y: "-=30",
+        duration: 1,
+      });
     }, comp);
     return () => ctx.revert();
   }, []);
   return (
     <div className="relative" ref={comp}>
-      <div className="h-screen absolute w-full" id="intro-slider">
+      <div className="h-screen absolute w-full hidden" id="intro-slider">
         <div className="h-full text-[#202020] justify-center lowercase text-9xl gap-10 flex flex-col">
           <div>nickey</div>
           <div>web3 Developer</div>
@@ -46,15 +40,17 @@ function Body() {
             have them
           </div>
           <div className="w-full text-[#202020] lowercase text-[24px] gap-1 flex flex-col items-end">
-            <div>nickey</div>
-            <div>web3 Developer</div>
-            <button className="italic w-min">email</button>
+            <div>greey works</div>
+            <div>Design & Development studio </div>
+            <button className="italic w-min" onClick={handleEmailClick}>
+              email
+            </button>
             <div className="flex">
               {/* Socials */}
               <div className="flex py-1 items-center gap-2">
                 <SocialButton
                   name={"twitter"}
-                  url="https://twitter.com/nickeym"
+                  url="https://twitter.com/ingreey"
                 />
                 <SocialButton
                   name={"linkedin"}
@@ -62,7 +58,7 @@ function Body() {
                 />
                 <SocialButton
                   name={"github"}
-                  url="https://github.com/nickeym"
+                  url="https://github.com/ingreey"
                 />
               </div>
             </div>
